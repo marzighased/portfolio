bd23a68e289e093429dea71c5978a8dab3e0e221
marzi ghased <marzighased@gmail.com>
2025-01-23 17:01:02 +0100

    about me section added

--- src/app/main-content-landingpage/hero-section/hero-section.component.ts
+++ src/app/main-content-landingpage/hero-section/hero-section.component.ts
@@ -15,17 +15,23 @@ interface MenuItem {
   styleUrl: './hero-section.component.scss'
 })
 export class HeroSectionComponent {
+  isMenuOpen = false;
+
   menuItems: MenuItem[] = [
     { label: 'About me', href: '#about', active: false },
     { label: 'Skills', href: '#skills', active: false },
     { label: 'Projects', href: '#projects', active: false }
   ];
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
   setActiveItem(index: number): void {
-    this.menuItems = this.menuItems.map((item, i) => ({
-      ...item,
-      active: i === index
-    }));
+    this.menuItems.forEach((item, i) => {
+        item.active = i === index;
+    });
+    this.isMenuOpen = false;
   }
 }
 
--- src/app/main-content-landingpage/main-content-landingpage.component.ts
+++ src/app/main-content-landingpage/main-content-landingpage.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
 
 @Component({
   selector: 'app-main-content-landingpage',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent],
+  imports: [CommonModule, HeroSectionComponent, AboutMeSectionComponent],
   templateUrl: './main-content-landingpage.component.html',
   styleUrl: './main-content-landingpage.component.scss'
 })
