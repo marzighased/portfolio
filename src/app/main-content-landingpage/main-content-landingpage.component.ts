c3785f137bfcbe871134ad9191dbf09d17b69a77
marzi ghased <marzighased@gmail.com>
2025-01-26 10:38:55 +0100

    skills added

--- src/app/main-content-landingpage/main-content-landingpage.component.ts
+++ src/app/main-content-landingpage/main-content-landingpage.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @Component({
   selector: 'app-main-content-landingpage',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent, AboutMeSectionComponent],
+  imports: [CommonModule, HeroSectionComponent, AboutMeSectionComponent,SkillsSectionComponent],
   templateUrl: './main-content-landingpage.component.html',
   styleUrl: './main-content-landingpage.component.scss'
 })
